82964a68e8e694c568df0013b5ec16f21f13d995
Daniele <prihood@bk.ru>
2022-06-09 18:06:58 +0300

    add Modal on Third page

--- app/src/pages/ThirdPage/index.jsx
+++ app/src/pages/ThirdPage/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../../components/Button";
+import Modal from "../../components/Modal";
+import Slider from "../../components/Slider";
 
 import styles from "./ThirdPage.module.scss";
 
 const ThirdPage = () => {
+  const [isModalActive, setIsModalActive] = useState(false);
+
   return (
     <div className={`${styles.wrapper} backgroundImage`}>
       <div className={styles.content}>
@@ -30,7 +34,47 @@ const ThirdPage = () => {
               A arcu <br /> cursus vitae
             </p>
           </div>
-          <Button icon="plus">Подробнее</Button>
+          <Button icon="plus" onClick={() => setIsModalActive(true)}>
+            Подробнее
+          </Button>
+          <Modal active={isModalActive} setActive={setIsModalActive}>
+            <div className="modalTop">
+              <span className={`mainSuptitle ${styles.suptitle}`}>
+                преимущества
+              </span>
+              <h2 className="mainTitle">
+                BREND<span className="mainTitleBold">XY</span>
+              </h2>
+            </div>
+            <div className="counterParent">
+              <Slider width="500px" height="333px">
+                <div>
+                  <div className="listItem">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                  </div>
+                  <div className="listItem">
+                    <p>Faucibus pulvinar elementum integer enim</p>
+                  </div>
+                  <div className="listItem">
+                    <p>Faucibus pulvinar elementum integer enim</p>
+                  </div>
+                </div>
+                <div>
+                  <div className="listItem">
+                    <p>Mi bibendum neque egestas congue quisque egestas diam</p>
+                  </div>
+                  <div className="listItem">
+                    <p>Venenatis lectus magna fringilla urna</p>
+                  </div>
+                  <div className="listItem">
+                    <p>Venenatis lectus magna fringilla urna</p>
+                  </div>
+                </div>
+              </Slider>
+            </div>
+          </Modal>
         </div>
       </div>
       <div className={`${styles.bottleImg} backgroundImage`} />
